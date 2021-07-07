1293af529297928bf499a0d7d37a7ca5143cf70a
nlessmann <773597+nlessmann@users.noreply.github.com>
2021-07-07 16:42:11 +0200

    Update interactive.js

--- static/assets/js/interactive.js
+++ static/assets/js/interactive.js
@@ -1,9 +1,11 @@
 jQuery(function($) {
+    /* Open navigation bar on mobile devices */
     let $nav = $('#sidebar nav ul');
     $('#sidebar nav .toggle button').click(function() {
         $nav.slideToggle('fast');
     });
 
+    /* Buttons and links that show or hide another elements */
     $('.toggle_visibility').click(function() {
         let self = $(this);
         let selector = self.data('selector');
@@ -22,5 +24,13 @@ jQuery(function($) {
         }
 
         rootElement.slideToggle('fast');
+    });
+
+    /* Sponsor logos, switch height for maximum height */
+    $('div.logos img').each(function() {
+        let self = $(this);
+        let height = self.attr('height');
+        self.css('height', 'auto');
+        self.css('max-height', height + 'px');
     });
 });
